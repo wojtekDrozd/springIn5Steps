5d17a068688e4c41b3f205c008d67c4c87c4369e
wojtekDrozd <wojdro89@gmail.com>
2018-12-10 22:26:25 +0100

    Specific Annotatnions added

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/componentscan/ComponentDAO.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/componentscan/ComponentDAO.java
@@ -1,11 +1,9 @@
 package com.in28minutes.spring.basics.componentscan;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-//import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 //@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class ComponentDAO {
 	
--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BubbleSortAlgorithm.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BubbleSortAlgorithm.java
@@ -1,10 +1,9 @@
 package com.in28minutes.spring.basics.springin5steps.basic;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @Qualifier("bubble")
 public class BubbleSortAlgorithm implements SortAlgorithm {
 	public int[] sort(int[] numbers) {
